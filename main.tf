13165f415dec48f851dff952e86ff586ca7e6142
Yeh0natan <107115371+Yeh0natan@users.noreply.github.com>
2023-09-01 15:54:24 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -34,6 +34,18 @@
             to_port = 443
             cidr_blocks = ["0.0.0.0/0"]
         }
+	ingress {
+            from_port = 80
+            protocol = "tcp"
+            to_port = 3000
+            cidr_blocks = ["0.0.0.0/0"]
+        }
+	ingress {
+            from_port = 80
+            protocol = "tcp"
+            to_port = 3001
+            cidr_blocks = ["0.0.0.0/0"]
+        }
         egress {
             from_port       = 0
             to_port         = 0
